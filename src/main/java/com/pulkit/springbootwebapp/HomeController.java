3f0ad3ea9cef22b08827cce8bdc9340d29dd9267
Pulkit Aggarwal <pulkit.aggarwal@nagarro.com>
2022-07-29 11:47:06 +0530

    Use @RequestParam and add ModelView instead of sesssion

--- src/main/java/com/pulkit/springbootwebapp/HomeController.java
+++ src/main/java/com/pulkit/springbootwebapp/HomeController.java
@@ -5,17 +5,17 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("home")
-	public String home(HttpServletRequest req) {
-		HttpSession session = req.getSession();
-		String name = req.getParameter("name");
-		System.out.println("Hii, this is home page " + name);
-		session.setAttribute("name", name); //setting in session so that we can use in jsp page
-		return "home"; // return file under webapp home.jsp, default folder name is webapp
-		// spring boot convert the jsp into servelet internally
+	public ModelAndView home(@RequestParam("name") String myName) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("name", myName);
+		mv.setViewName("home");
+		return mv; 
 	}
 }
